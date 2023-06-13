09c575d323a9c8124433da9bd36baeb71fc50e0f
Yusuf Çolak <yusuf21706@gmail.com>
2023-06-13 18:49:21 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+
+const server = require("./api/server.js");
+
+const port = 9000;
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+  });
+  
